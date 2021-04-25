aef0e8e0a91ea4462a2bae1d20930b1822508e1e
DNibs <dnibs513@gmail.com>
2021-04-25 13:24:57 -0400

    added prints, basic analysis, system solver

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 # Example script for EE302 students unfamiliar with python
 # Author: MAJ Niblick
 # Date: 25APR2021
+# Requires Python 3.8X or greater, numpy
 
 # First are import statements which tell Python to use additional libraries.
 # Specifically, we will import math for basic math funcitons, and cmath for complex functions.
 
 import math
 import cmath
+import numpy as np
+import sympy as sym
 
 
-# Next we define some basic helper functions of our own
+# Next we define some custom helper functions
 
-# By default, python uses radians instead of degrees. To avoid having to convert from degrees to
+# By default, python uses radians instead of degrees. To avoid manually converting from degrees to
 #   radians every time we want to input a polar number, this function simplifies it for us.
 def polar(magnitude, degrees):
     """Converts polar number (magnitude, angle in degrees) to complex cartesian number"""
@@ -19,12 +22,88 @@ def polar(magnitude, degrees):
 
 
 def print_polar(complex_number, variable_name='', units='', significant_digits=3):
-    """Prints complex number as polar in degrees with optional variable name and units"""
+    """Prints complex number as polar in scientific format in degrees with optional variable name and units"""
+    if variable_name != '':
+        var_name = variable_name + ' = '
+    else:
+        var_name = ''
     mag = abs(complex_number)
     angle = math.degrees(cmath.polar(complex_number)[1])
-    print(f'{variable_name} = {mag:.{significant_digits}e} angle {angle:.{significant_digits}e} {units}')
+    print(f'{var_name}{mag:.{significant_digits-1}e} angle {angle:.{significant_digits-1}e} deg {units}')
 
 
-if __name__ == '__main__':
-    vt = complex(5, 2)
-    print_polar(vt, variable_name='Vt', units='V')
+def print_cartesian(complex_number, variable_name='', units='', significant_digits=3):
+    """Prints complex number as polar in scientific format in degrees with optional variable name and units"""
+    if variable_name != '':
+        var_name = variable_name + ' = '
+    else:
+        var_name = ''
+    print(f'{var_name}{complex_number.real:.{significant_digits-1}e} + j'
+          f'{complex_number.imag:.{significant_digits-1}e} {units}')
+
+
+# Script stars here -------------------------------------------------------------------------------
+print('Part 1: Initializing and Printing Complex Numbers -----------------')
+# "complex" function is how you input a complex number. The first input is real, the second is imaginary
+vt = complex(5, 0)
+
+# Here are a few ways to print the complex variable. First is default print statement.
+# The other three use our custom functions above, with the last also using the optional arguments.
+print(vt)
+print_cartesian(vt)
+print_polar(vt)
+print_polar(vt, variable_name='vt', units='V', significant_digits=5)
+
+# Now we will define a variable by its polar coordinates instead of its cartesian coordinates using our custom function
+it = polar(.25, 30)
+print(it)
+print_cartesian(it)
+print_polar(it, variable_name='it', units='A', significant_digits=4)
+
+# Let's analyze some circuits!
+print('Part 2: Basic Analysis --------------------')
+# Say we have a simple circuit with a voltage source of 5angle0 V at 100 rad/s, a resistor of 100ohms,
+#   and a capacitor of 82 microFarads, all in series.
+vs = polar(5, 0)
+radians_per_second = 100
+r1 = 100
+c1 = 82e-6
+z_c = complex(0, -1/(radians_per_second*c1))
+
+# To find the voltage across the resistor, we can voltage divider
+v_r1 = vs * r1 / (r1 + z_c)
+print_polar(v_r1, variable_name='v_r1', units='V')
+
+print('Part 3: Solving Systems --------------------')
+# To solve a system of equations for nodal or mesh analysis, we can use sympy's solve function
+# Set up your equations as a matrix multiplication, like the following:
+# Say we have to solve a circuit with the following equations
+# eqn1 = (va-5)/r1 + (va-0)/r2 + (va-vb)/z_c == 0
+# eqn2 = (vb-va)/z_c + (vb-0)/r3
+r1 = 100
+r2 = 200
+r3 = 150
+z_c = complex(0, -1/(100*82e-6))
+
+# Declare variables to solve
+va, vb = sym.symbols('va, vb')
+
+# Set up equations
+eqn1 = sym.Eq((va-5)/r1 + (va-0)/r2 + (va-vb)/z_c, 0)
+eqn2 = sym.Eq((vb-va)/z_c + vb/r3, 0)
+
+# Solve equations ad save the solution as an object called "solution"
+solution = sym.solve([eqn1, eqn2], (va, vb))
+print(solution)
+
+# To call out a specific variable from solution, use solution[variable]
+print(solution[va])
+
+# Notice that solution is actually formatted as a string, not a complex number. To do use the solution
+#   as a complex number from this point on, we can convert it with the 'complex()' function
+va = complex(solution[va])
+print(va)
+print_polar(va, variable_name='va', units='V')
+
+# Let's plot some results!
+print('Part 4: Plotting ------------------------')
